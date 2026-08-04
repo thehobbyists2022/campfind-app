ba660630417110899d16050761474fee94c701bd
thehobbyists2022 <thehobbyists2022@gmail.com>
2026-08-04 16:03:34 -0700

    chore: bump Android release to version 1.0.2 (code 5)

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
     }
 
     signingConfigs {
