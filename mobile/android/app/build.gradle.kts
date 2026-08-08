433c7b90196425c83f215e7cfacf76c6a823e92e
thehobbyists2022 <thehobbyists2022@gmail.com>
2026-08-07 23:09:17 -0700

    chore: bump Android release to 1.0.8 (code 11)
    - Rebuilt signed AAB with 4,171-camp dataset (39 ACA-accredited, incl.
      6 new under-represented-state camps)
    - CampFind-v1.0.8-release.aab; removed stale v1.0.6 build artifact

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 10
-        versionName = "1.0.7"
+        versionCode = 11
+        versionName = "1.0.8"
     }
 
     signingConfigs {
