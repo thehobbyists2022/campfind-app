48057a76aa192537594da2506c6395329d13e09c
thehobbyists2022 <thehobbyists2022@gmail.com>
2026-08-07 21:26:40 -0700

    chore: bump Android release to 1.0.7 (code 10)
    - Rebuilt signed AAB with 4,165-camp dataset (34 ACA-accredited +
      57 Bricks 4 Kidz)
    - CampFind-v1.0.7-release.aab; removed stale v1.0.5 build artifact

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 9
-        versionName = "1.0.6"
+        versionCode = 10
+        versionName = "1.0.7"
     }
 
     signingConfigs {
