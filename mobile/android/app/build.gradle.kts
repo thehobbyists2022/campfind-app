158f5a613159c9d4009f3ebc478d982cce362959
thehobbyists2022 <thehobbyists2022@gmail.com>
2026-08-09 06:37:44 -0700

    chore: bump Android release to 1.0.9 (code 12)
    - Rebuilt signed AAB with 4,314-camp dataset (incl. 47 new franchise
      locations + Minneapolis city camps)
    - CampFind-v1.0.9-release.aab; removed stale v1.0.7 build artifact
    - Can update directly from 1.0.3 on Play (versionCode 6 -> 12 is fine)

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 11
-        versionName = "1.0.8"
+        versionCode = 12
+        versionName = "1.0.9"
     }
 
     signingConfigs {
